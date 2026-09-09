bc99e2f1e2e4a82190d4b8c88f406166ac454a8e
Melek Doghmen <melek.doghmen@etudiant-fst.utm.tn>
2026-09-09 15:50:12 +0100

    Time: 0 ms (100%), Space: 8.3 MB (11.5%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,24 +1,10 @@
 class Solution {
+    vector <int> dp;
 public:
     int fib(int n) {
-       /* vector <int> dp(n+1,-1);
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        else{
-            if(n<=1){
-                return dp[n]=n;
-            }
-            else{
-             dp[n]=fib(n-1)+fib(n-2);  
-        } return dp[n];}
-        */
-        if(n<=1){
-            return n;
-        }
-        else{
-
-            return fib(n-1)+fib(n-2);
-        }
+        if ((int)dp.size() < n+1) dp.assign(n+1, -1);
+        if (dp[n] != -1) return dp[n];
+        if (n <= 1) return dp[n] = n;
+        return dp[n] = fib(n-1) + fib(n-2);
    }
 };
